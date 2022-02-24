e72a394464e46fe680a48994bf26f96731d48d7d
alou28 <jean.alain.ngomedj@efrei.net>
2022-02-24 15:37:58 +0100

    set up connection with the backend server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from axios;
+import { text } from 'express';
 
-function App() {
+function post_text() {
+
+
+try{
+  await axios.post('http://localhost:4000/post_text',
+  text
+  
+  )
+} catch(error){
+  console.log(error)
+}
+
+
+
   return (
 <body>
 <div class="container">
     <div class="container-lg">
         <h1 class="title"> Sentiment Analysis </h1>
-        <form method="POST" class="form">
-          <textarea type="text" name="inp" placeholder="Enter your feelings ! " id="inp"></textarea>
+        <form method="POST" onSubmit={post_text}>
+          <textarea type="text" value={text} name="inp" placeholder="Enter your feelings ! " id="inp"></textarea>
             <input type="submit" name="submit" id="btn" class="btn btn-primary"></input>
 
         </form>
