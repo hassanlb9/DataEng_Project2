afa0553dafeb2646b759c10866b56bc8c7babd24
BACHACHAHassan <93646318+hassanlb9@users.noreply.github.com>
2022-02-26 04:37:42 +0100

    Add files via upload

---
+++ k/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [text,setText]=useState("");
+  async function post_text(e){
+
+try{
+
+  await axios.post("http://localhost:3000/post_text",
+  {text}
+  
+  )
+} catch(error){
+  console.log(error)
+}
+  }
+
+
+  return (
+<body>
+<div class="container">
+    <div class="container-lg">
+        <h1 class="title"> Toxic Monitor</h1>
+        <form method="POST" onSubmit={post_text}>
+          <textarea type="text" value={text} name="inp" placeholder="Enter your feelings ! " id="inp"></textarea>
+            <input type="submit" name="submit" id="btn" class="btn btn-primary"></input>
+
+        </form>
+     
+       
+
+    </div>
+</div>
+</body>
+  );
+}
+
+export default App;
