6f1d78c6bfe00db44a5bfbd7a5fe56a47d4bfa04
alou28 <jean.alain.ngomedj@efrei.net>
2022-03-04 18:45:36 +0100

    tests on the server side

---
+++ tests.py
@@ -0,0 +1,46 @@
+import warnings
+from main import app
+warnings.filterwarnings("ignore")
+import unittest
+import logging
+import time
+from detoxify import Detoxify
+
+# Use log instead of print
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+CLASSES = [
+    "toxicity",
+    "severe_toxicity",
+    "obscene",
+    "threat",
+    "insult",
+    "identity_attack",
+    "sexual_explicit",
+]
+
+
+class TestApi(unittest.TestCase):
+
+    URL = "http://localhost:5000/post_text"
+
+    def test_server_is_up(self):
+        tester=app.test_client(self)
+        response = tester.get("/test_text")
+        statuscode=response.status_code
+        self.assertEqual(statuscode,200)
+
+   
+
+    def test_original(self):
+        model = Detoxify("original")
+        results = model.predict(["shut up, you liar", "you look like Marilyn Monroe"])
+        assert len(results) == 6
+        assert all(cl in results for cl in CLASSES[:6])
+        assert results["toxicity"][0] >= 0.7
+        assert results["toxicity"][1] < 0.5
+
+            
+if __name__ == '__main__':
+    unittest.main()
